268ec97c0fdc5638c6447f060e2a96cb476b47eb
layal Abdullah <layal1426@outlook.sa>
2026-02-04 11:31:49 +0300

    resolve

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 export default defineConfig({
+  base: '/React-Portfoio/',     
   plugins: [react()],
-  build: {
-    outDir: 'docs'
-  }
+
 })
